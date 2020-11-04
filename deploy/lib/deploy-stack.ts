ca91166d0ab28a7a87ba0923cb515053f0f4687e
Ashwanth A R <ashwanth1109@gmail.com>
2020-11-04 10:50:15 +0530

    deploy: create s3 auto delete bucket

--- deploy/lib/deploy-stack.ts
+++ deploy/lib/deploy-stack.ts
@@ -1,9 +1,14 @@
 import * as cdk from '@aws-cdk/core';
+import { AutoDeleteBucket } from '@mobileposse/auto-delete-bucket';
 
 export class DeployStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    new AutoDeleteBucket(this, 'deno-scripts', {
+      bucketName: 'deno-scripts'
+    })
   }
 }
